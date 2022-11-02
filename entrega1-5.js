4644622b4be6a80dc006b417123c274c48cb8851
NicolasPucar <114008069+NicolasPucar@users.noreply.github.com>
2022-11-02 08:49:00 +0100

    Update entrega1-5.js
    Arreglado tema encapsulación y añadido ejercicio recursividad.

--- entrega1-5.js
+++ entrega1-5.js
@@ -1,42 +1,69 @@
-/* - Exercici 1
+/* /* - Exercici 1
 Crea una funció que, en executar-la, escrigui una frase en un fitxer.
 
  */
+const fs = require(`fs`);
 
-const fs = require(`fs`)
-
-const archivo = `prueba.txt`
+const archivo = `prueba.txt`;
 
 if (fs.existsSync(archivo)) {
-  //Me aseguro de que el archivo no existe
-  console.log(`El archivo ya existe`)
+	console.log(`El archivo ya existe`);
 } else {
-  console.log(`El archivo no existe`)
+	console.log(`El archivo no existe`);
 }
-const contenido = `texto de prueba`
-fs.writeFileSync(archivo, contenido)
+const contenido = `texto de prueba que acabará saliendo en consola`;
+const creator = () => {
+	fs.writeFileSync(archivo, contenido);
+};
+creator();
 
-/* 
-- Exercici 2
+/* - Exercici 2
 Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior. */
 
-const showOnconsole = () => {
-  fs.readFile('prueba.txt', 'utf8', (error, data) => {
-    if (error) {
-      console.log(`Algo ha fallado: ${error}`)
-    } else {
-      console.log(data)
-    }
-  })
+function showOnconsole() {
+	fs.readFile('prueba.txt', 'utf8', (error, data) => {
+		if (error) {
+			console.log(`Algo ha fallado: ${error}`);
+		} else {
+			console.log(data);
+		}
+	});
 }
-showOnconsole()
 
-/*     - Exercici 3
-    Crea una funció que comprimeixi el fitxer del nivell 1. */
+showOnconsole();
 
-var zlib = require('zlib')
+/*   - Exercici 3
+    Crea una funció que comprimeixi el fitxer del nivell 1.  */
 
-var gzip = zlib.createGzip()
-var sincomprimir = fs.createReadStream('./prueba.txt')
-var comprimido = fs.createWriteStream('./pruebaComprimida.txt.gz')
-sincomprimir.pipe(gzip).pipe(comprimido)
+let zlib = require('zlib');
+let tipoGzip = zlib.createGzip();
+let archivoPruebaNom = './prueba.txt';
+function compresor() {
+	const lector = fs.createReadStream(archivoPruebaNom);
+	const comprimidoCreador = fs.createWriteStream('./pruebaComprimida.txt.gz');
+	lector.pipe(tipoGzip).pipe(comprimidoCreador);
+}
+compresor();
+
+/* - Exercici 1
+Crea una funció que imprimeixi recursivament 
+un missatge per la consola amb demores d'un segon. */
+
+//El reloj se detendrá 4 veces por minuto, dependiendo de la hora exacta
+let reloj = () => {
+	const ahora = new Date();
+	const ahoraHoras = ahora.getHours();
+	const ahoraMinutos = ahora.getMinutes();
+	const ahoraSegundos = ahora.getSeconds();
+
+	let hora = `${ahoraHoras}:${ahoraMinutos}:${ahoraSegundos}`;
+	if (ahoraSegundos === 05) return;
+	if (ahoraSegundos === 20) return;
+	if (ahoraSegundos === 35) return;
+	if (ahoraSegundos === 50) return;
+	setTimeout(reloj, 1000);
+	console.log(hora);
+};
+
+reloj();
+
